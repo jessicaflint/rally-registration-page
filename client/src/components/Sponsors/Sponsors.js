b2f238918daf400440708295993e076d0df6522f
jose31canizar <jose.canizares@colorado.edu>
2017-11-03 14:35:37 -0600

    edit css for sponsors

--- client/src/components/Sponsors/Sponsors.js
+++ client/src/components/Sponsors/Sponsors.js
@@ -4,11 +4,13 @@ import './Sponsors.styl'
 class Sponsors extends Component {
   render() {
     return (
-      <div className='sponsors'>
+      <div className='sponsors-container'>
         <h2>Thanks to our sponsors</h2>
+        <div className='sponsors'>
         <img src={require(`../../img/logo-aurora.jpg`)}/>
         <img src={require(`../../img/logo-green-mountain-fox-run.png`)}/>
         <img src={require(`../../img/logo-reasons.jpg`)}/>
+        </div>
       </div>
     );
   }
